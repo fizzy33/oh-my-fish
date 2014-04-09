dfd0b554fbddd70ad52002249f90143e5d3dcee1
Glen Marchesani <glen@model3.net>
2014-04-09 19:24:45 -0400

    upgraded glen's prompt

--- themes/glen/fish_prompt.fish
+++ themes/glen/fish_prompt.fish
@@ -1,14 +1,17 @@
 # name: RobbyRussel
 function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+  echo (command git symbolic-ref HEAD ^/dev/null)
 end
 
 function fish_prompt
-  
+  # Save our status
+  set -l last_status $status
+
+  set -l last_status_string ""
+  if [ $last_status -ne 0 ]
+    set last_status_string (printf " %s(%d)%s " (set_color red --bold) $last_status (set_color normal))
+  end
+
   set -l black_on_white (set_color -b white black)
 
   set -l white_bg (set_color -b white)
@@ -20,39 +23,39 @@ function fish_prompt
   set -l magenta (set_color -o magenta)
   set -l purple (set_color -o purple)
   set -l cyan (set_color -o cyan)
+  set -l green (set_color green)
   set -l yellow (set_color -o yellow)
   set -l red (set_color -o red)
   set -l blue (set_color -o blue)
   set -l normal (set_color normal)
 
-  set -l short_hostname (hostname -s)
 
-  set -l user_hostname "$blue$USER$normal$grey@$purple$short_hostname"
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_user_hostname
+    set -l short_hostname (hostname -s)
+    set __fish_prompt_user_hostname "$blue$USER$normal$grey@$purple$short_hostname"
+  end
+
   set -l arrow "$normal$grey:"
 
 #  set -l cwd $cyan(basename (prompt_pwd))
   set -l cwd $cyan(pwd)
 
   if [ (_git_branch_name) ]
-    set -l git_branch $red(_git_branch_name)
-    set git_info "$blue git($git_branch$blue)"
+    # sed is removing leading spaces
+    set git_info {" "}{$blue}git(__fish_git_prompt | sed -e 's/^[ \t]*//')  
 
-    if [ (_is_git_dirty) ]
-      set -l dirty "$yellow ✗"
-      set git_info "$git_info$dirty"
-    end
   end
 
   set -l window ""
   if test -z $WINDOW
     set window ""
   else 
-    set window "screen($WINDOW)" 
+    set window "$green screen($white$WINDOW$normal$green)" 
   end
 
   echo " "
-  echo -s (date +%X) $window $git_info 
-  echo -n -s $user_hostname $arrow $cwd ' ↪ '
+  echo -s (date +%X) $window $git_info $last_status_string
+  echo -n -s $__fish_prompt_user_hostname $arrow $cwd $white ' ↪ '
 
 end
-
