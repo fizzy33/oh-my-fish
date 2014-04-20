8fa8f42dabd5027857086dd8b51e797371d6178b
Glen Marchesani <glen@model3.net>
2014-04-19 21:26:10 -0400

    moved git prompt colors into repo

--- themes/emeris/fish_title.fish
+++ themes/emeris/fish_title.fish
@@ -4,3 +4,11 @@ function fish_title
   set -l user_hostname "$USER@$short_hostname:$cwd"
   echo $user_hostname
 end
+
+
+set __fish_git_prompt_showdirtystate enabled
+set __fish_git_prompt_show_informative_status enabled
+set __fish_git_prompt_showstashstate enabled
+set __fish_git_prompt_showupstream verbose
+set __fish_git_prompt_showcolorhints enabled
+
